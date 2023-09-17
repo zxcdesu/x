2863124fd947f3c914475b1b8a28a94d064a92b9
xanedesu <xanedesu@gmail.com>
2023-09-17 18:37:51 +0300

    history

---
+++ apps/history/src/main.ts
@@ -0,0 +1,25 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen();
+  Logger.log('🚀 Application is running');
+}
+
+bootstrap();
