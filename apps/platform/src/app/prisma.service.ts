bc1db25c49427dcfbc415c5480ba502e0ee19508
xanedesu <xanedesu@gmail.com>
2023-11-22 19:18:46 +0300

    build

--- apps/platform/src/app/prisma.service.ts
+++ apps/platform/src/app/prisma.service.ts
@@ -1,18 +1,11 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../prisma/generated';
 
 export * from '../../prisma/generated';
-export { Decimal } from '../../prisma/generated/runtime/library';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
   }
 }
