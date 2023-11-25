f635d51f9612ef8518e71368320610e6b259188d
xanedesu <xanedesu@gmail.com>
2023-11-25 11:27:10 +0300

    todo access

--- apps/auth/src/app/project/project-jwt-payload.interface.ts
+++ apps/auth/src/app/project/project-jwt-payload.interface.ts
@@ -3,5 +3,6 @@ import { UserJwtPayload } from '../user/user-jwt-payload.interface';
 export interface ProjectJwtPayload extends UserJwtPayload {
   project: {
     id: number;
+    // TODO: access
   };
 }
