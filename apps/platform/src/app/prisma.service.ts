24ee4a0b258f2184634d64902e1a3cc0f2078b46
xanedesu <xanedesu@gmail.com>
2024-01-28 20:40:19 +0300

    rm comment

--- apps/platform/src/app/prisma.service.ts
+++ apps/platform/src/app/prisma.service.ts
@@ -5,8 +5,6 @@ export * from '../../prisma/generated';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  // TODO: prisma-redis-middleware
-
   async onModuleInit() {
     await this.$connect();
   }
