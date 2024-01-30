b038c10a528e983ccf9d091b8e102f2528367a3e
xanedesu <xanedesu@gmail.com>
2024-01-30 16:10:58 +0300

    notifications

---
+++ apps/notifications/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import {
+  ClassSerializerInterceptor,
+  Module,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import joi from 'joi';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        DATABASE_URL: joi.string().uri().required(),
+        BROKER_URL: joi.string().uri().required(),
+        TELEGRAM_BOT_TOKEN: joi.string().required(),
+      }),
+    }),
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        enableControllerDiscovery: true,
+        uri: configService.get<string>('BROKER_URL'),
+        exchanges: [
+          {
+            name: 'notifications',
+            type: 'topic',
+          },
+        ],
+        prefetchCount: 1,
+        connectionInitOptions: {
+          wait: false,
+        },
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+    },
+  ],
+})
+export class AppModule {}
