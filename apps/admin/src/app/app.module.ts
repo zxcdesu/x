68c16b2f1a8e74d61ed57b81365818f6d2d153ea
xanedesu <xanedesu@gmail.com>
2024-02-26 23:52:58 +0300

    admin

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -5,11 +5,11 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import joi from 'joi';
 import { AdminController } from './admin/admin.controller';
 import { AdminService } from './admin/admin.service';
 import { PrismaService } from './prisma.service';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -24,7 +24,7 @@ import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         enableControllerDiscovery: true,
-        uri: configService.get<string>('BROKER_URL'),
+        uri: configService.getOrThrow<string>('BROKER_URL'),
         exchanges: [
           {
             name: 'admin',
