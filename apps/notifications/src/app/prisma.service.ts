0d3fab2ceb05985d3ad0d35846024deb285878f3
xanedesu <xanedesu@gmail.com>
2024-02-27 00:07:14 +0300

    notifications

--- apps/notifications/src/app/prisma.service.ts
+++ apps/notifications/src/app/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../prisma/generated';
 
-export * from '../../prisma/generated';
+export { Prisma, Subscriber, SubscriberProvider } from '../../prisma/generated';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
