52600e363730ade54be92b022ac4ac4ae9053de7
xanedesu <xanedesu@gmail.com>
2024-05-12 18:57:15 +0300

    admin

--- apps/admin/src/app/admin/admin.controller.ts
+++ apps/admin/src/app/admin/admin.controller.ts
@@ -7,7 +7,7 @@ export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @AdminRmq.check()
-  check(@UserId() userId: number) {
+  check(@UserId() userId: number): Promise<boolean> {
     return this.adminService.check(userId);
   }
 }
